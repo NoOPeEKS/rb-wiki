6361909053895aab52e18a459b6f68be761bb585
NoOPeEKS <arnauapps@gmail.com>
2024-07-16 14:21:55 +0200

    Add category column to Articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_16_072412) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_16_120749) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
   create_table "users", force: :cascade do |t|
