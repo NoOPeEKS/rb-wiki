9699cd47e34bc74a799753a399443702ccaed242
NoOPeEKS <arnauapps@gmail.com>
2024-10-04 12:22:45 +0200

    Start markdown construction of Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,24 @@
+require 'reverse_markdown'
+
 class Article < ApplicationRecord
+  after_create :generate_markdown_file
   belongs_to :category
-  has_rich_text :text
+  has_rich_text :body
+
+  def generate_markdown_file
+    markdown_content = construct_markdown
+    puts markdown_content
+  end
+
+  def construct_markdown
+    markdown_body = ReverseMarkdown.convert(body)
+
+    <<-MARKDOWN
+      # #{title}
+      **Author**: John Doe
+
+      ## Body:
+      #{markdown_body}
+    MARKDOWN
+  end
 end
