a25250ff19a99a8e0427570dcc017fff84bc9004
NoOPeEKS <arnauapps@gmail.com>
2024-10-04 09:16:53 +0200

    Update proposal fields datatypes from text to string

--- db/migrate/20241002083120_create_proposals.rb
+++ db/migrate/20241002083120_create_proposals.rb
@@ -4,18 +4,18 @@ class CreateProposals < ActiveRecord::Migration[7.1]
       t.string :title
       t.string :tipus
       t.string :call
-      t.text :app_domain
+      t.string :app_domain
       t.date :deadline
       t.integer :duration
       t.string :status
-      t.text :client
-      t.text :units_involved
-      t.text :people_involved
+      t.string :client
+      t.string :units_involved
+      t.string :people_involved
       t.integer :total_budget
       t.integer :eurecat_budget
       t.string :eurecat_budget_excel
       t.string :document_link
-      t.text :keywords
+      t.string :keywords
 
       t.timestamps
     end
