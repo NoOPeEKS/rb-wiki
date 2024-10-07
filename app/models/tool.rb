90d3dc69e1561b1497a8b7ccc4cccd277de7bfbf
NoOPeEKS <arnauapps@gmail.com>
2024-10-07 12:25:43 +0200

    Markdown representation of Tool model added

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,4 +1,29 @@
+require 'reverse_markdown'
+
 class Tool < ApplicationRecord
+  after_create :generate_markdown_file
   has_rich_text :usage
   belongs_to :user
+
+  def generate_markdown_file
+    markdown_content = construct_markdown
+    puts markdown_content
+  end
+
+  def construct_markdown
+    markdown_usage = ReverseMarkdown.convert(usage)
+
+    <<-MARKDOWN
+    # Title: #{title}
+    ## Type: #{tipus}
+    ## Date: #{date}
+    ## Repository link: #{code_repo_link}
+    ## Document link: #{document_link}
+    ## Units involved: #{units}
+    ## Keywords: #{keywords}
+    ## Technology: #{technology}
+    ## Usage:
+    #{markdown_usage}
+    MARKDOWN
+  end
 end
