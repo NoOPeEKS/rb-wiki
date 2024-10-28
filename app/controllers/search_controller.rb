1b4de7d123a8a3d3bdd0f83dd5176530e7973ccf
NoOPeEKS <arnauapps@gmail.com>
2024-10-28 12:41:01 +0100

    feat: complete fuzzyfinding for articles, tools, projects and proposals

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,21 @@
 class SearchController < ApplicationController
   def search
     @query = params[:query]
-    @articles = Article.where("title LIKE ? OR body LIKE ?", "%#{@query}%", "%#{@query}%")
-    @projects = Project.where("title LIKE ?", "%#{@query}%")
-    @proposals = Proposal.where("title LIKE ?", "%#{@query}%")
-    @tools = Tool.where("title LIKE ?", "%#{@query}%")
+    @articles = Article.where(
+      "title LIKE ? OR body LIKE ?",
+      "%#{@query}%", "%#{@query}%")
+    @projects = Project.where(
+      "title LIKE ? OR tipus LIKE ? OR call LIKE ? OR app_domain LIKE ? OR client LIKE ? OR keywords LIKE ?",
+      "%#{@query}%","%#{@query}%", "%#{@query}%", "%#{@query}%", "%#{@query}%", "%#{@query}%"
+    )
+    @proposals = Proposal.where(
+      "title LIKE ? OR tipus LIKE ? OR call LIKE ? OR app_domain LIKE ? OR keywords LIKE ?",
+      "%#{@query}%", "%#{@query}%", "%#{@query}%", "%#{@query}%", "%#{@query}%"
+    )
+    @tools = Tool.where(
+      "title LIKE ? OR tipus LIKE ? OR keywords LIKE ?",
+      "%#{@query}%", "%#{@query}%", "%#{@query}%"
+    )
     render :search_results
   end
 end
