7ee486ddbbe03134c74853a5cae68cfdb9f29f21
NoOPeEKS <arnauapps@gmail.com>
2024-12-11 09:41:25 +0100

    feat: chat now calls rag microservice api and returns response

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,8 +1,27 @@
+require 'net/http'
+
 class ChatController < ApplicationController
   def index
   end
 
   def send_message
-    render json: { response: "ola"}
+    begin
+      recieved_data = JSON.parse(request.raw_post)
+      message = recieved_data["message"]
+
+      puts("Missatge rebut: #{message}")
+      api_url = Rails.configuration.api_config['api_url']
+      uri = URI("#{api_url}/chat/ask?query=#{URI.encode_www_form_component(message)}")
+      http = Net::HTTP.new(uri.host, uri.port)
+      request = Net::HTTP::Post.new(uri)
+      response = http.request(request)
+
+      puts response.body
+
+
+      render json: { response: message }, status: :ok
+    rescue JSON::ParserError
+      render json: { error: "Invalid JSON" }, status: :unprocessable_entity
+    end
   end
 end
